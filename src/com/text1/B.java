4f92293365a696560473b2b68a26f228c1416dcf
xiaoyuganya <1006700776@qq.com>
2020-06-23 15:02:55 +0800

    嘿嘿

--- src/com/text1/B.java
+++ src/com/text1/B.java
@@ -4,7 +4,12 @@ public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("   * *   * *    ");
+		System.out.println(" * * * * * * *  ");
+		System.out.println("  * * * * * *   ");
+		System.out.println("    * * * *     ");
+		System.out.println("      * *       ");
+		System.out.println("       *        ");
 	}
 
 }
